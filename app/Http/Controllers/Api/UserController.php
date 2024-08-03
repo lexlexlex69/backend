2c629ede9929aae67fff2c0102960bc2d2edb194
lex <you@example.com>
2024-08-03 15:31:47 +0800

    added image column and migrate, image upload

--- app/Http/Controllers/Api/UserController.php
+++ app/Http/Controllers/Api/UserController.php
@@ -7,6 +7,7 @@ use Illuminate\Http\Request;
 use App\Http\Requests\UserRequest;
 use Illuminate\Support\Facades\Hash;
 use App\Models\User;
+use Illuminate\Support\Facades\Storage;
 
 class UserController extends Controller
 {
@@ -86,6 +87,19 @@ class UserController extends Controller
     {
         $user = User::findOrFail($id);
         $user->delete();
+
+        return $user;
+    }
+
+    public function image(UserRequest $request, string $id)
+    {
+        $user = User::findOrFail($id);
+        if(!is_null($user->image)){
+            Storage::disk('public')->delete($user->image);
+        }
+        $user->image = $request->file('image')->storePublicly('images', 'public');
+
+        $user->save();
 
         return $user;
     }
